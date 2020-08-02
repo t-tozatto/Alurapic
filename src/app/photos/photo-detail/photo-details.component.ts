c843b70d7f91163841722ee4570f860008e843bd
Thaís Tozatto <thais.c.tozatto@gmail.com>
2020-08-02 15:06:53 -0300

    Componente de notificação.

--- src/app/photos/photo-detail/photo-details.component.ts
+++ src/app/photos/photo-detail/photo-details.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { PhotoService } from '../photo/photo.service';
 import { Photo } from '../photo/photo';
 import { Observable } from 'rxjs';
+import { AlertService } from '../../shared/components/alert/alert.service';
+import { UserService } from '../../core/user/user.service';
 
 @Component({
     templateUrl: './photo-details.component.html'
@@ -13,15 +15,27 @@ export class PhotoDetailsComponent implements OnInit {
 
     constructor(private route: ActivatedRoute,
                 private photoService: PhotoService,
-                private router: Router) {}
+                private router: Router,
+                private alertService: AlertService,
+                private userService: UserService) {}
 
     ngOnInit(): void {
         this.photo$ = this.photoService.findById(this.route.snapshot.params.photoId);
+        this.photo$.subscribe(() => {}, err => {
+            this.router.navigate(['not-found']);
+        });
     }
 
     remove(photoId: number): void {
         this.photoService
             .removePhoto(photoId)
-            .subscribe(() => this.router.navigate(['']));
+            .subscribe(
+                () => {
+                    this.alertService.success('Photo removed', true);
+                    this.router.navigate(['/user', this.userService.getUserName()]);
+                },
+                err => {
+                    this.alertService.warning('Could not delete the photo!');
+                });
     }
 }
--- src/app/photos/photo-form/photo-form.component.ts
+++ src/app/photos/photo-form/photo-form.component.ts
@@ -2,15 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { PhotoService } from '../photo/photo.service';
 import { Router } from '@angular/router';
+import { AlertService } from '../../shared/components/alert/alert.service';
+import { UserService } from '../../core/user/user.service';
 
 @Component({
   // tslint:disable-next-line: component-selector
   selector: 'ap-photo-form',
   templateUrl: './photo-form.component.html',
-  styleUrls: ['./photo-form.component.css']
+  styleUrls: ['./photo-form.component.css'],
 })
 export class PhotoFormComponent implements OnInit {
-
   photoForm: FormGroup;
   file: File;
   preview: string;
@@ -18,14 +19,16 @@ export class PhotoFormComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
     private photoService: PhotoService,
-    private router: Router
-  ) { }
+    private router: Router,
+    private alertService: AlertService,
+    private userService: UserService
+  ) {}
 
   ngOnInit(): void {
     this.photoForm = this.formBuilder.group({
       file: ['', Validators.required],
       description: ['', Validators.maxLength(300)],
-      allowComments: [true]
+      allowComments: [true],
     });
   }
 
@@ -34,13 +37,16 @@ export class PhotoFormComponent implements OnInit {
     const allowComments = this.photoForm.get('allowComments').value;
     this.photoService
       .upload(description, allowComments, this.file)
-      .subscribe(() => this.router.navigate(['']));
+      .subscribe(() => {
+        this.alertService.success('Upload complete', true);
+        this.router.navigate(['/user', this.userService.getUserName()]);
+      });
   }
 
   handleFile(file: File): void {
     this.file = file;
     const fileReader = new FileReader();
-    fileReader.onload = (event: any) => this.preview = event.target.result;
+    fileReader.onload = (event: any) => (this.preview = event.target.result);
     fileReader.readAsDataURL(file);
   }
 }
